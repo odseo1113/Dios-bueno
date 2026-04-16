408529121951ab9ae52208b7243d57df0aa8a6b4
Oswin Segura <odeso1113@gmail.com>
2026-04-15 19:48:51 -0500

    fix init definitivo

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,14 @@
 from flask import Flask
-from database import init_db  # 🔥 IMPORTANTE
+from .database import init_db
 
 def create_app():
     app = Flask(__name__)
 
-    # 🔐 CLAVE PARA LOGIN (SESIONES)
+    # 🔐 CLAVE PARA LOGIN
     app.secret_key = "superclave123"
 
     # 🔥 INICIALIZAR BASE DE DATOS
     init_db()
-
-    from database import crear_tabla_respuestas
-crear_tabla_respuestas()
 
     from .routes import main
     app.register_blueprint(main)
